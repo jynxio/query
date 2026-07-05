0bd8b5c4053da2907be8ebf4c2176a7495318582
jynxio <jinxiaomatrix@gmail.com>
2026-07-06 00:30:07 +0800

    fix: Incorrect operator

--- src/_misc/schedule-task.ts
+++ src/_misc/schedule-task.ts
@@ -2,11 +2,11 @@ const MIN_TIMEOUT = 0;
 const MAX_TIMEOUT = 2_147_483_647;
 
 function scheduleTask(task: () => void, timeout: number): () => void {
-    if (timeout <= MIN_TIMEOUT) throw new RangeError(`"timeout" must be >= ${MIN_TIMEOUT}`);
+    if (timeout < MIN_TIMEOUT) throw new RangeError(`"timeout" must be >= ${MIN_TIMEOUT}`);
     if (timeout >= MAX_TIMEOUT) throw new RangeError(`"timeout" must be <= ${MAX_TIMEOUT}`);
 
     /**
-     *  Via setTimeout
+     * Via settimeout API.
      */
     if (timeout > 10) {
         const handle = setTimeout(task, timeout);
@@ -16,7 +16,7 @@ function scheduleTask(task: () => void, timeout: number): () => void {
     }
 
     /**
-     * Via AbortSignal
+     * Via AbortSignal API.
      */
     const handle = AbortSignal.timeout(timeout);
     const cancelTask = () => handle.removeEventListener("abort", task);

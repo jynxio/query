f800ef38ec50288bbefde9869d1b09fdbf42c31d
jynxio <jinxiaomatrix@gmail.com>
2026-07-11 00:19:13 +0800

    fix: Handle sync errors in 'withSafe'

--- src/_hooks/with-safe.ts
+++ src/_hooks/with-safe.ts
@@ -1,6 +1,6 @@
 import type { QueryError } from "../_error.ts";
 import type { Safe } from "../_types.ts";
-import type { QueryPromise } from "../_promise.ts";
+import { QueryPromise } from "../_promise.ts";
 
 import { toQueryError } from "../_misc/transformers.ts";
 
@@ -8,9 +8,13 @@ function withSafe<Args extends unknown[], R>(
     fn: (...args: Args) => QueryPromise<R>,
 ): (...args: Args) => QueryPromise<Safe<R, QueryError>> {
     return function (...args: Args): QueryPromise<Safe<R, QueryError>> {
-        return fn(...args)
-            .then((res) => ({ ok: true, data: res }) as const)
-            .catch((err) => ({ ok: false, error: toQueryError(err) }) as const);
+        try {
+            return fn(...args)
+                .then((res) => ({ ok: true, data: res }) as const)
+                .catch((err) => ({ ok: false, error: toQueryError(err) }) as const);
+        } catch (unknown) {
+            return QueryPromise.resolve({ ok: false, error: toQueryError(unknown) });
+        }
     };
 }
 

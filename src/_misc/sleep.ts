a6615a8ac5e034a188659a4e0699f3d8ac5b7267
jynxio <jinxiaomatrix@gmail.com>
2026-07-11 17:55:02 +0800

    fix: Use 'signal.reason' when signal is aborted in 'sleep'

--- src/_misc/sleep.ts
+++ src/_misc/sleep.ts
@@ -1,23 +1,22 @@
-import { QueryError } from "../_error.ts";
 import { scheduleTask } from "./schedule-task.ts";
 
 function sleep(duration: number, signal?: AbortSignal): Promise<void> {
-    if (signal?.aborted) return Promise.reject(new QueryError("abort"));
+    if (signal?.aborted) throw signal.reason;
 
-    const handle = Promise.withResolvers<void>();
+    const ctrl = Promise.withResolvers<void>();
     const cancelResolve = scheduleTask(resolve, duration);
 
     signal?.addEventListener("abort", reject, { once: true });
 
-    return handle.promise;
+    return ctrl.promise;
 
     function reject() {
         cancelResolve();
-        handle.reject(new QueryError("abort"));
+        ctrl.reject(signal?.reason);
     }
 
     function resolve() {
-        handle.resolve();
+        ctrl.resolve();
         signal?.removeEventListener("abort", reject);
     }
 }

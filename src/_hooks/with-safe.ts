6c689d6f9ca386fad57e5213ce6f6bd411f66a41
jynxio <jinxiaomatrix@gmail.com>
2026-07-13 21:35:16 +0800

    chore: Add a comment

--- src/_hooks/with-safe.ts
+++ src/_hooks/with-safe.ts
@@ -5,6 +5,7 @@ import { QueryPromise } from "../_promise.ts";
 function withSafe<Args extends unknown[], R>(
     fn: (...args: Args) => QueryPromise<R>,
 ): (...args: Args) => QueryPromise<Safe<R, unknown>> {
+    // Do not catch synchronous exceptions from `fn(...args)`, so invalid `new QueryRequest` calls can terminate the program.
     return function (...args: Args): QueryPromise<Safe<R, unknown>> {
         return fn(...args)
             .then<{ ok: true; data: R }>((data) => ({ ok: true, data }))
